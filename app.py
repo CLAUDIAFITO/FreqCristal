e8fb8a5e0cff38879dc6ea20d4ad023de80c46ac
CLAUDIAFITO <reis.castillo@gmail.com>
2026-01-07 13:46:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import wave
 from datetime import date, timedelta
 from typing import Dict, List, Tuple, Any, Optional
 
+import math
+
 import numpy as np
 import pandas as pd
 import streamlit as st
